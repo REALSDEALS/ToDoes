b5722397f0ba807bd338c377b5ea7cac3427baf8
REALSDEALS <jesselamberink@MacBook-Air-van-Jesse.local>
2021-03-22 16:30:05 +0100

    Implement an navigation tab #4

---
+++ todoes/lib/nav.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:todoes/homescreen.dart';
+
+class Nav extends StatefulWidget {
+  @override
+  _NavState createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  int _SelectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Text('To-Does'),
+    Text('Calendar'),
+  ];
+
+  void _OnItemTap(int index) {
+    setState(() {
+      _SelectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('ToDoes!'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_SelectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check),
+            label: 'To-Does',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today_rounded),
+            label: 'Calendar',
+          ),
+        ],
+        currentIndex: _SelectedIndex,
+        onTap: _OnItemTap,
+        selectedItemColor: Colors.red,
+      ),
+    );
+  }
+}
